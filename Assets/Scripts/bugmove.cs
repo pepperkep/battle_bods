3e9c99ca758781e998e9a7969ee249490de8f8e7
Austin Keppers <akeppers21@gmail.com>
2021-06-12 19:32:34 -0400

    Fixed gun cycle

--- Assets/Scripts/bugmove.cs
+++ Assets/Scripts/bugmove.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class bugmove : Movement
 {
+    public GameObject target;
+    public float minWaitAmount = 0.5f;
+    public float maxWaitAmount = 2.5f;
+    private IEnumerator hopRoutine;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = gameObject.GetComponent<Rigidbody>();
+       hopRoutine = Hop(minWaitAmount, maxWaitAmount);
+       StartCoroutine(hopRoutine);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        /*
+        Vector3 moveDirection = Vector2.left;
+        if(!moveTowards)
+            moveDirection = Vector2.right;
+        nextMove = moveDirection;
+        this.animator.SetFloat("speed", Mathf.Abs(moveDirection.x));
+        */
+    }
+
+    IEnumerator Hop(float minDuration, float maxDuration)
+    {
+        while(true)
+        {
+            float timeToSwitch = Random.Range(minDuration, maxDuration);
+            yield return new WaitForSeconds(timeToSwitch);
+            Jump();
+        }
     }
 }
