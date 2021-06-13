6b46f2da4279afeb530fa1ba62b8ae15622c0c09
Austin Keppers <akeppers21@gmail.com>
2021-06-13 02:56:20 -0400

    Added drag drop system

--- Assets/Scripts/DragDropManager.cs
+++ Assets/Scripts/DragDropManager.cs
@@ -7,62 +7,83 @@ public class DragDropManager : MonoBehaviour
 {
     private Rigidbody enemyPart;
     private Vector3 originalPosition;
-    private Vector2 mouseMove;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private bool isDraggable;
-    bool isGrabbed = false;
+    private Vector2 mousePosition;
+    private Vector2 offset;
     bool canAttach = true;
     Vector2 pos;
+    private GameObject selectedObject;
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-       enemyPart = gameObject.GetComponent<Rigidbody>(); 
-        
+       mainCamera = Camera.main;
     }
-    void FixedUpdate()
+    void Update()
     {
-        
-             pos = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
-            //Debug.Log(pos);
-            
-            
-            
-        
+        pos = mainCamera.ScreenToViewportPoint(Mouse.current.position.ReadValue());
     }
     
-    public void onGrabPart(InputAction.CallbackContext context){
-        if(isDraggable){
-        isGrabbed = true;
-        Debug.Log("Found enemy part!");
-        originalPosition = transform.position;  
-        Debug.Log(originalPosition);
+    public void onGrabPart(InputAction.CallbackContext context)
+    {
+        pos = mainCamera.ScreenToViewportPoint(mousePosition);
+        if(context.ReadValue<float>() > 0.5f)
+        {
+            mousePosition = Mouse.current.position.ReadValue();
+            Ray ray = mainCamera.ViewportPointToRay(pos);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.CompareTag("Arm"))
+                {
+                    selectedObject = hit.transform.parent.gameObject;
+                    offset = (Vector2)(mainCamera.ScreenToWorldPoint(mousePosition) - selectedObject.transform.position);
+                    selectedObject.transform.SetParent(null);
+                    Damageable damageComponent = selectedObject.GetComponentInChildren<Damageable>();
+                    Damager damagerComponent = selectedObject.GetComponentInChildren<Damager>();
+                    Rigidbody rb = selectedObject.GetComponentInChildren<Rigidbody>();
+                    rb.isKinematic = true;
+                    damageComponent.isEnemy = false;
+                    damageComponent.isDead = false;
+                    damagerComponent.damageEnemy = true;
+                    damagerComponent.damagePlayer = false;
+                }
+            }
         }
+        else
+        {  
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(mainCamera.ViewportPointToRay(pos), 100.0f);
 
-    }
+            bool onBod = false;
+            GameObject bod = null;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+                if(hit.transform.CompareTag("Player"))
+                {
+                    onBod = true;
+                    bod = hit.transform.gameObject;
+                }
+            }
 
-    public void onDragPart(InputAction.CallbackContext context){
-    if(isDraggable && isGrabbed){
-    mouseMove = context.ReadValue<Vector2>();
-    enemyPart.MovePosition((enemyPart.position + ((Vector3)(mouseMove))));
-    }
-    }
-
-     private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.name == "Bod"){
-            Debug.Log("Found new body");
-            canAttach = true;
+            if (onBod)
+            {
+                selectedObject.transform.SetParent(bod.gameObject.transform, true);
+                selectedObject.GetComponent<Animator>().enabled = true;
+            }
+            selectedObject = null;
         }
     }
 
-    public void OnReleasePart(InputAction.CallbackContext context) {
-        if(canAttach){
-        isGrabbed = false;
-        isDraggable = false;
-         GameObject bod = GameObject.Find("Bod");
-        this.gameObject.transform.SetParent(bod.gameObject.transform, true);
-        //transform.parent.GetComponent<Animator>().enabled = true;
+    public void onDragPart(InputAction.CallbackContext context)
+    {
+        if(selectedObject != null){
+            Vector2 nextMousePosition = Mouse.current.position.ReadValue();
+            Vector3 mouseMove = mainCamera.ScreenToWorldPoint(nextMousePosition) - mainCamera.ScreenToWorldPoint(mousePosition);
+            mousePosition = nextMousePosition;
+            Vector3 nextWorldPos = mainCamera.ScreenToWorldPoint(nextMousePosition);
+            selectedObject.transform.position = new Vector3(nextWorldPos.x, nextWorldPos.y) - (Vector3)offset;
         }
-         
     }
-    
 }
